b050fd07bd6ed94ce5de95a3fd6ecdc46f824434
xiong-kaifang <xiongkaifang87@163.com>
2014-02-13 11:22:32 +0800

    Add osa cmd module.

---
+++ src/osa_cmd.c
@@ -0,0 +1,166 @@
+/** ============================================================================
+ *
+ *  Copyright (C), 1987 - 2014, xiong-kaifang Tech. Co.,, Ltd.
+ *
+ *  @File name:	osa_cmd.c
+ *
+ *  @Author: xiong-kaifang   Version: v1.0   Date: 2014-01-03
+ *
+ *  @Description:   The description of this file.
+ *	
+ *	                The template format for source file.
+ *
+ *  @Version:	    v1.0
+ *
+ *  @Function List:  //	主要函数及功能
+ *	    1.  －－－－－
+ *	    2.  －－－－－
+ *
+ *  @History:	     //	历史修改记录
+ *
+ *	<author>	    <time>	     <version>	    <desc>
+ *  xiong-kaifang   2014-01-03     v1.0	        write this module.
+ *
+ *
+ *  ============================================================================
+ */
+
+/*  --------------------- Include system headers ---------------------------- */
+
+/*  --------------------- Include user headers   ---------------------------- */
+#include "osa.h"
+#include "osa_cmd.h"
+#include "osa_task_mgr.h"
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+/*
+ *  --------------------- Macro definition -------------------------------------
+ */
+
+/** ============================================================================
+ *  @Macro:         Macro name
+ *
+ *  @Description:   Description of this macro.
+ *  ============================================================================
+ */
+
+/*
+ *  --------------------- Structure definition ---------------------------------
+ */
+
+/** ----------------------------------------------------------------------------
+ *  @Name:          Structure name
+ *
+ *  @Description:   Description of the structure.
+ *
+ *  @Field:         Field1 member
+ *
+ *  @Field          Field2 member
+ *  ----------------------------------------------------------------------------
+ */
+
+
+/*
+ *  --------------------- Global variable definition ---------------------------
+ */
+
+/** ----------------------------------------------------------------------------
+ *  @Name:          Variable name
+ *
+ *  @Description:   Description of the variable.
+ * -----------------------------------------------------------------------------
+ */
+
+/*
+ *  --------------------- Local function forward declaration -------------------
+ */
+
+/** ============================================================================
+ *
+ *  @Function:	    Local function forward declaration.
+ *
+ *  @Description:   //	函数功能、性能等的描述
+ *
+ *  @Calls:	        //	被本函数调用的函数清单
+ *
+ *  @Called By:	    //	调用本函数的函数清单
+ *
+ *  @Table Accessed://	被访问的表（此项仅对于牵扯到数据库操作的程序）
+ *
+ *  @Table Updated: //	被修改的表（此项仅对于牵扯到数据库操作的程序）
+ *
+ *  @Input:	        //	对输入参数的说明
+ *
+ *  @Output:	    //	对输出参数的说明
+ *
+ *  @Return:	    //	函数返回值的说明
+ *
+ *  @Enter          //  Precondition
+ *
+ *  @Leave          //  Postcondition
+ *
+ *  @Others:	    //	其它说明
+ *
+ *  ============================================================================
+ */
+
+/*
+ *  --------------------- Public function definition ---------------------------
+ */
+
+/** ============================================================================
+ *
+ *  @Function:	    Public function definition.
+ *
+ *  @Description:   //	函数功能、性能等的描述
+ *
+ *  @Calls:	        //	被本函数调用的函数清单
+ *
+ *  @Called By:	    //	调用本函数的函数清单
+ *
+ *  @Table Accessed://	被访问的表（此项仅对于牵扯到数据库操作的程序）
+ *
+ *  @Table Updated: //	被修改的表（此项仅对于牵扯到数据库操作的程序）
+ *
+ *  @Input:	        //	对输入参数的说明
+ *
+ *  @Output:	    //	对输出参数的说明
+ *
+ *  @Return:	    //	函数返回值的说明
+ *
+ *  @Enter          //  Precondition
+ *
+ *  @Leave          //  Postcondition
+ *
+ *  @Others:	    //	其它说明
+ *
+ *  ============================================================================
+ */
+status_t osa_cmd_request(osa_cmd_req_t *preq)
+{
+    if (preq == NULL) {
+        return OSA_EARGS;
+    }
+
+    return task_mgr_synchronize2(preq->m_id, (void *)preq, sizeof(*preq), MSG_FLAGS_WAIT_ACK);
+}
+
+/*
+ *  --------------------- Local function definition ----------------------------
+ */
+
+/** ============================================================================
+ *
+ *  @Function:	    Local function definition.
+ *
+ *  @Description:   //	函数功能、性能等的描述
+ *
+ *  ============================================================================
+ */
+
+#if defined(__cplusplus)
+}
+#endif
