18de2422342ac5bf3977d21e657ee8e9a4fee1da
xiong-kaifang <xiongkaifang87@163.com>
2015-09-18 10:45:21 +0800

    Create new thread detached.

--- osa/include/thread.h
+++ osa/include/thread.h
@@ -6,36 +6,40 @@
  *
  *  @Author: xiong-kaifang   Version: v1.0   Date: 2012-09-13
  *
- *  @Description:   //	用于详细说明此程序文件完成的主要功能，与其它模块
- *		            //	或函数的接口，输出值，取值范围、含义及参数间的控
- *		            //	制、顺序、独立或依赖等关系
- *		            //
+ *  @Description:   // 用于详细说明此程序文件完成的主要功能，与其它模块
+ *                  // 或函数的接口，输出值，取值范围、含义及参数间的控
+ *                  // 制、顺序、独立或依赖等关系
+ *                  //
  *
- *	                The header file thread.
+ *                  The header file thread.
  *
- *  @Others:	    //	其它内容说明
+ *  @Others:        //	其它内容说明
  *
  *  @Function List: //	主要函数列表，每条记录就包括函数名及功能简要说明
- *	    1.  ...
- *	    2.  ...
+ *      1.  ...
+ *      2.  ...
  *
- *  @History:	    //	修改历史记录列表，每条修改记录就包括修改日期、修改
- *	        	    //	时间及修改内容简述
+ *  @History:       // 修改历史记录列表，每条修改记录就包括修改日期、修改
+ *                  // 时间及修改内容简述
  *
- *	<author>	    <time>	     <version>	    <desc>
+ *  <author>        <time>       <version>      <description>
+ *
  *  xiong-kaifang   2012-09-13     v1.0	        Write this module.
+ *
+ *  xiong-kaifang   2015-09-18     v1.2         1. Delete 'thread_handle'
+ *                                                 datatype, using 'thread_t'.
  *
  *  ============================================================================
  */
 
-#if !defined (THREAD_H_)
-#define THREAD_H_
+#if !defined (__OSA_THREAD_H)
+#define __OSA_THREAD_H
 
 /*  --------------------- Include system headers ---------------------------- */
-#include "std_defs.h"
 
 /*  --------------------- Include user headers   ---------------------------- */
-
+#include "std_defs.h"
+#include "osa_status.h"
 
 #if defined(__cplusplus)
 extern "C" {
@@ -52,12 +56,11 @@ extern "C" {
  *  @Description:   Description of this macro.
  *  ============================================================================
  */
-#define MAILBOX_MAX_MSG_SIZE    (64)
 
 /*
  *  --------------------- Data type definition ---------------------------------
  */
-struct __thread_t; typedef struct __thread_t thread_t, *thread_handle;
+typedef HANDLE      thread_t;
 
 /** ----------------------------------------------------------------------------
  *  @Name:          Structure name
@@ -96,60 +99,60 @@ extern thread_attrs_t default_thd_attrs;
 
 /** =============================================================================
  *
- *  @Function:	    //	函数名称
+ *  @Function:      // 函数名称
  *
- *  @Description:   //	函数功能、性能等的描述
+ *  @Description:   // 函数功能、性能等的描述
  *
- *  @Calls:	        //	被本函数调用的函数清单
+ *  @Calls:	        // 被本函数调用的函数清单
  *
- *  @Called By:	    //	调用本函数的函数清单
+ *  @Called By:	    // 调用本函数的函数清单
  *
- *  @Table Accessed://	被访问的表（此项仅对于牵扯到数据库操作的程序）
+ *  @Table Accessed:// 被访问的表（此项仅对于牵扯到数据库操作的程序）
  *
- *  @Table Updated: //	被修改的表（此项仅对于牵扯到数据库操作的程序）
+ *  @Table Updated: // 被修改的表（此项仅对于牵扯到数据库操作的程序）
  *
- *  @Input:	        //	对输入参数的说明
+ *  @Input:	        // 对输入参数的说明
  *
- *  @Output:	    //	对输出参数的说明
+ *  @Output:        // 对输出参数的说明
  *
- *  @Return:	    //	函数返回值的说明
+ *  @Return:        // 函数返回值的说明
  *
- *  @Enter          //  Precondition
+ *  @Enter          // Precondition
  *
- *  @Leave          //  Postcondition
+ *  @Leave          // Postcondition
  *
- *  @Others:	    //	其它说明
+ *  @Others:        // 其它说明
  *
  *  ============================================================================
  */
-thread_handle   thread_create(Fxn fxn, thread_attrs_t * attrs, ...);
+status_t     thread_create(thread_t *pthd, Fxn fxn, thread_attrs_t * attrs, ...);
 
-void            thread_delete(thread_handle hdl);
+status_t     thread_delete(thread_t *pthd);
 
-//int             thread_get_pri(thread_handle hdl);
+//int        thread_get_pri(thread_t thd);
 
-void *          thread_get_env(thread_handle hdl);
+void *       thread_get_env(thread_t thd);
 
-const char *    thread_get_name(thread_handle hdl);
+const char * thread_get_name(thread_t thd);
 
-//int             thread_set_pri(thread_handle hdl, int newpri);
+//int        thread_set_pri(thread_t thd, int newpri);
 
-//void *          thread_set_env(thread_handle hdl, void *env);
+//void *     thread_set_env(thread_t thd, void *env);
 
-const char *    thread_set_name(thread_handle hdl, char *name);
+const char * thread_set_name(thread_t thd, char *name);
 
-thread_handle   thread_self(void);
+thread_t     thread_self(void);
 
-int             thread_cancel(thread_handle hdl);
+int          thread_cancel(thread_t thd);
 
-int             thread_join(thread_handle hdl);
+int          thread_join(thread_t thd);
 
-//int             thread_stat(thread_handle hdl);
+//int          thread_stat(thread_t thd);
 
-void            thread_yield(void);
+void         thread_yield(void);
 
 #if defined(__cplusplus)
 }
 #endif  /* defined(__cplusplus) */
 
-#endif  /* if !defined (THREAD_H_) */
+#endif  /* if !defined (__OSA_THREAD_H) */
--- osa/src/thread.c
+++ osa/src/thread.c
@@ -7,21 +7,27 @@
  *  @Author: xiong-kaifang   Version: v1.0   Date: 2012-09-13
  *
  *  @Description:   The thread.
- *	
  *
- *  @Version:	    v1.0
  *
- *  @Function List:  //	主要函数及功能
- *	    1.  －－－－－
- *	    2.  －－－－－
+ *  @Version:       v1.0
  *
- *  @History:	     //	历史修改记录
+ *  @Function List: // 主要函数及功能
+ *      1.  －－－－－
+ *      2.  －－－－－
  *
- *	<author>	    <time>	     <version>	    <desc>
+ *  @History:       // 历史修改记录
+ *
+ *  <author>        <time>       <version>      <description>
+ *
  *  xiong-kaifang   2012-09-13     v1.0	        Write this module.
  *
  *  xiong-kaifang   2015-08-22     v1.1         Destroy thread attributes
  *                                              object.
+ *
+ *  xiong-kaifang   2015-09-18     v1.2         1. Create new thread detached.
+ *                                              2. Add 'THREAD_DEBUG' macro.
+ *                                              2. Delete 'thread_handle'
+ *                                                 datatype, using 'thread_t'.
  *
  *  ============================================================================
  */
@@ -56,6 +62,26 @@ extern "C" {
  */
 #define THREAD_MAX_ARGS (4)
 
+#define thread_check_arguments(arg)         osa_check_arguments(arg)
+
+#define thread_check_arguments2(arg1, arg2) osa_check_arguments2(arg1, arg2)
+
+/** ============================================================================
+ *  @Macro:         THREAD_DEBUG
+ *
+ *  @Description:   Description of this macro.
+ *  ============================================================================
+ */
+//#define THREAD_DEBUG
+
+#if !defined(THREAD_DEBUG)
+
+#ifdef DBG
+#undef DBG
+#define DBG(level, tag, arg...)
+#endif
+
+#endif  /* if defined THREAD_DEBUG */
 /*
  *  --------------------- Structure definition ---------------------------------
  */
@@ -70,20 +96,22 @@ extern "C" {
  *  @Field          Field2 member
  *  ----------------------------------------------------------------------------
  */
-struct __thread_t {
+struct __thread_t
+{
     DLIST_ELEMENT_RESERVED;
-    pthread_t           thd_id;
-    pthread_attr_t      pattrs;
 
-    Fxn                 fxn;
-    Arg                 args[THREAD_MAX_ARGS];
+    pthread_t       thd_id;
+    pthread_attr_t  pattrs;
 
-    int                 pri;
-    char              * name;
-    int                 thd_status;
-    int                 exit_status;
+    Fxn             fxn;
+    Arg             args[THREAD_MAX_ARGS];
 
-    void              * arg;
+    int             pri;
+    char          * name;
+    int             thd_status;
+    int             exit_status;
+
+    void          * env;
 };
 
 /*
@@ -103,8 +131,9 @@ thread_attrs_t default_thd_attrs = {
     NULL
 };
 
-static int              cur_init = 0;
-static pthread_key_t    thd_key;
+static int                glb_cur_init = 0;
+
+static pthread_key_t      glb_thd_key;
 
 static const char * const GT_NAME = "thread";
 /*
@@ -113,7 +142,7 @@ static const char * const GT_NAME = "thread";
 
 /** ============================================================================
  *
- *  @Function:	    Local function forward declaration.
+ *  @Function:      Local function forward declaration.
  *
  *  @Description:   //	函数功能、性能等的描述
  *
@@ -127,21 +156,21 @@ static const char * const GT_NAME = "thread";
  *
  *  @Input:	        //	对输入参数的说明
  *
- *  @Output:	    //	对输出参数的说明
+ *  @Output:        //	对输出参数的说明
  *
- *  @Return:	    //	函数返回值的说明
+ *  @Return:        //	函数返回值的说明
  *
  *  @Enter          //  Precondition
  *
  *  @Leave          //  Postcondition
  *
- *  @Others:	    //	其它说明
+ *  @Others:        //	其它说明
  *
  *  ============================================================================
  */
 static void   __thread_cleanup(void *hdl);
 
-static void * __thread_run_stub(thread_handle hdl);
+static void * __thread_run_stub(struct __thread_t *pthd);
 
 /*
  *  --------------------- Public function definition ---------------------------
@@ -149,227 +178,230 @@ static void * __thread_run_stub(thread_handle hdl);
 
 /** ============================================================================
  *
- *  @Function:	    Public function definition.
+ *  @Function:      Public function definition.
  *
- *  @Description:   //	函数功能、性能等的描述
+ *  @Description:   // 函数功能、性能等的描述
  *
- *  @Calls:	        //	被本函数调用的函数清单
+ *  @Calls:	        // 被本函数调用的函数清单
  *
- *  @Called By:	    //	调用本函数的函数清单
+ *  @Called By:	    // 调用本函数的函数清单
  *
- *  @Table Accessed://	被访问的表（此项仅对于牵扯到数据库操作的程序）
+ *  @Table Accessed:// 被访问的表（此项仅对于牵扯到数据库操作的程序）
  *
- *  @Table Updated: //	被修改的表（此项仅对于牵扯到数据库操作的程序）
+ *  @Table Updated: // 被修改的表（此项仅对于牵扯到数据库操作的程序）
  *
- *  @Input:	        //	对输入参数的说明
+ *  @Input:	        // 对输入参数的说明
  *
- *  @Output:	    //	对输出参数的说明
+ *  @Output:        // 对输出参数的说明
  *
- *  @Return:	    //	函数返回值的说明
+ *  @Return:        // 函数返回值的说明
  *
- *  @Enter          //  Precondition
+ *  @Enter          // Precondition
  *
- *  @Leave          //  Postcondition
+ *  @Leave          // Postcondition
  *
- *  @Others:	    //	其它说明
+ *  @Others:        // 其它说明
  *
  *  ============================================================================
  */
-int             thread_init(void)
+int          thread_init(void)
 {
-    if (cur_init++ == 0) {
-        pthread_key_create(&thd_key, NULL);
+    if (glb_cur_init++ == 0) {
+        pthread_key_create(&glb_thd_key, NULL);
     }
 
     return 0;
 }
 
-void            thread_exit(void)
+void         thread_exit(void)
 {
-    if (--cur_init == 0 ) {
-        pthread_key_delete(thd_key);
+    if (--glb_cur_init == 0 ) {
+        pthread_key_delete(glb_thd_key);
     }
 }
 
-thread_handle   thread_create(Fxn fxn, thread_attrs_t * attrs, ...)
+status_t     thread_create(thread_t *pthd, Fxn fxn, thread_attrs_t * attrs, ...)
 {
-    int           i;
-    va_list       va;
-    thread_handle thd_hdl = NULL;
+    int                 i;
+    va_list             va;
+    status_t            status = OSA_SOK;
+    struct __thread_t * thd    = NULL;
 
-    DBG(DBG_DETAILED, GT_NAME, "thread_create: Enter (fxn=0x%x, attrs=0x%x)\n", 
-            fxn, attrs);
+    thread_check_arguments2(pthd, fxn);
 
-    if (attrs == NULL) {
-        attrs = &default_thd_attrs;
+    (*pthd) = INVALID_HANDLE;
+
+    status = OSA_memAlloc(sizeof(struct __thread_t), &thd);
+    if (OSA_ISERROR(status) || thd == NULL) {
+        return status;
     }
 
-    OSA_memAlloc(sizeof(*thd_hdl), &thd_hdl);
+    thd->pri         = attrs->priority;
 
-    if (thd_hdl != NULL) {
-        thd_hdl->pri   = attrs->priority;
+    /*
+     *  TODO: Bug here, the attrs variable may be exist at stack,
+     *        the thd_hdl->name pointer may be invalid soon.
+     */
+    thd->name        = attrs->name;
 
-        /*
-         *  TODO: Bug here, the attrs variable may be exist at stack,
-         *        the thd_hdl->name pointer may be invalid soon.
-         */
-        thd_hdl->name  = attrs->name;
+    thd->env         = attrs->environ;
+    thd->fxn         = fxn;
+    thd->thd_status  = -1;
+    thd->exit_status = -1;
 
-        thd_hdl->arg   = attrs->environ;
+    /* Copy up to 'THREAD_MAX_ARGS' arguments from task */
+    va_start(va, attrs);
+    for (i = 0; i < OSA_ARRAYSIZE(thd->args); i++) {
+        thd->args[i] = va_arg(va, Arg);
+    }
+    va_end(va);
 
-        thd_hdl->fxn   = fxn;
+    /* Create a realtime detached thread */
+    pthread_attr_init(&thd->pattrs);
+    pthread_attr_setdetachstate(&thd->pattrs, PTHREAD_CREATE_DETACHED);
 
-        thd_hdl->thd_status  = -1;
-        thd_hdl->exit_status = -1;
+    thd->thd_status = pthread_create(&thd->thd_id, &thd->pattrs,
+            (void * (*)(void *))__thread_run_stub, (void *)thd);
+    if (thd->thd_status != 0) {
+        thread_delete((thread_t *)&thd);
+    }
 
-        /* Copy up to 8 arguments from task */
-        va_start(va, attrs);
-        for (i = 0; i < OSA_ARRAYSIZE(thd_hdl->args); i++) {
-            thd_hdl->args[i] = va_arg(va, Arg);
+    (*pthd) = (thread_t)thd;
+
+    return status;
+}
+
+status_t     thread_delete(thread_t *pthd)
+{
+    status_t            status = OSA_SOK;
+    struct __thread_t * thd    = (struct __thread_t *)(*pthd);
+
+    DBG(DBG_DETAILED, GT_NAME, "thread_delete: Enter (pthd=0x%x).\n", thd);
+
+    thread_check_arguments2(pthd, thd);
+
+    unsigned short cancel = (thd->thd_status == 0);
+
+    if (((thread_t)thd) != thread_self()) {
+        if (cancel) {
+            int r;
+
+            r = pthread_cancel(thd->thd_id);
+            r = pthread_join(thd->thd_id, NULL);
         }
-        va_end(va);
-
-        /* Create a realtime thread */
-        pthread_attr_init(&thd_hdl->pattrs);
-
-        thd_hdl->thd_status = pthread_create(&thd_hdl->thd_id, &thd_hdl->pattrs,
-             (void * (*)(void *))__thread_run_stub, (void *)thd_hdl);
-        if (thd_hdl->thd_status != 0) {
-            thread_delete(thd_hdl);
-            thd_hdl = NULL;
-        }
     }
 
-    DBG(DBG_DETAILED, GT_NAME, "thread_create: Exit (hdl=0x%x)\n", thd_hdl);
+    /*
+     *  Modified by: xiong-kaifang.
+     *
+     *  Date       : Aug 22, 2015.
+     *
+     *  Description:
+     *
+     *      Destroy thread attributes object.
+     *
+     */
+    pthread_attr_destroy(&thd->pattrs);
 
-    return thd_hdl;
+    status |= OSA_memFree(sizeof(struct __thread_t), thd);
+
+    (*pthd) = INVALID_HANDLE;
+
+    DBG(DBG_DETAILED, GT_NAME, "thread_delete: Exit.\n");
+
+    return status;
 }
 
-void            thread_delete(thread_handle hdl)
+//int        thread_get_pri(thread_t thd);
+
+void *       thread_get_env(thread_t thd)
 {
-    DBG(DBG_DETAILED, GT_NAME, "thread_delete: Enter (hdl=0x%x)\n", hdl);
+    struct __thread_t * pthd = (struct __thread_t *)thd;
 
-    if (hdl != NULL) {
-        unsigned short cancel = (hdl->thd_status == 0);
-
-        if (hdl != thread_self()) {
-            if (cancel) {
-                int r;
-
-                r = pthread_cancel(hdl->thd_id);
-                r = pthread_join(hdl->thd_id, NULL);
-            }
-        }
-
-        /*
-         *  Modified by: xiong-kaifang.
-         *
-         *  Date       : Aug 22, 2015.
-         *
-         *  Description:
-         *
-         *      Destroy thread attributes object.
-         *
-         */
-        pthread_attr_destroy(&hdl->pattrs);
-
-        OSA_memFree(sizeof(*hdl), hdl);
-    }
-
-    DBG(DBG_DETAILED, GT_NAME, "thread_delete: Exit\n");
+    return (pthd != NULL ? pthd->env : NULL);
 }
 
-//int             thread_get_pri(thread_handle hdl);
-
-void *          thread_get_env(thread_handle hdl)
+const char * thread_get_name(thread_t thd)
 {
-    return (hdl != NULL ? hdl->arg : NULL);
+    struct __thread_t * pthd = (struct __thread_t *)thd;
+
+    return (pthd != NULL ? pthd->name : NULL);
 }
 
-const char *    thread_get_name(thread_handle hdl)
+//int        thread_set_pri(thread_t thd, int newpri);
+
+//void *     thread_set_env(thread_t thd, void *env);
+
+const char * thread_set_name(thread_t thd, char *name)
 {
-    return (hdl != NULL ? hdl->name : NULL);
-}
+    char              * orign_name = NULL;
+    struct __thread_t * pthd = (struct __thread_t *)thd;
 
-//int             thread_set_pri(thread_handle hdl, int newpri);
-
-//void *          thread_set_env(thread_handle hdl, void *env);
-
-const char *    thread_set_name(thread_handle hdl, char *name)
-{
-    char * orign_name = NULL;
-
-    if (hdl != NULL) {
-        orign_name = hdl->name;
-        hdl->name  = name;
+    if (pthd != NULL) {
+        orign_name = pthd->name;
+        pthd->name = name;
     }
 
     return orign_name;
 }
 
-thread_handle   thread_self(void)
+thread_t     thread_self(void)
 {
-    thread_handle   hdl = NULL;
+    struct __thread_t * pthd = NULL;
 
-    DBG(DBG_DETAILED, GT_NAME, "thread_self: Enter\n");
+    DBG(DBG_DETAILED, GT_NAME, "thread_self: Enter.\n");
 
-    hdl = (thread_handle)pthread_getspecific(thd_key);
+    pthd = (struct __thread_t *)pthread_getspecific(glb_thd_key);
 
-    DBG(DBG_DETAILED, GT_NAME, "thread_self: Exit (hdl=0x%x)\n", hdl);
+    DBG(DBG_DETAILED, GT_NAME, "thread_self: Exit (pthd=0x%x).\n", pthd);
 
-    return hdl;
+    return ((thread_t)pthd);
 }
 
-int             thread_cancel(thread_handle hdl)
+int          thread_cancel(thread_t thd)
 {
-    int     status = 0;
+    int                status = OSA_SOK;
+    struct __thread_t * pthd = (struct __thread_t *)thd;
 
-    DBG(DBG_DETAILED, GT_NAME, "thread_cancel: Enter (hdl=0x%x)\n", hdl);
+    DBG(DBG_DETAILED, GT_NAME, "thread_cancel: Enter (thd=0x%x).\n", thd);
 
-    if (hdl == NULL) {
-        DBG(DBG_ERROR, GT_NAME, "thread_cancel: Invalid arguments\n");
-        return OSA_EINVAL;
-    }
+    thread_check_arguments(pthd);
 
-    status = pthread_cancel(hdl->thd_id);
+    status |= pthread_cancel(pthd->thd_id);
 
-    DBG(DBG_DETAILED, GT_NAME, "thread_cancel: Exit (status=0x%x)\n", status);
+    DBG(DBG_DETAILED, GT_NAME, "thread_cancel: Exit (status=0x%x).\n", status);
 
     return status;
 }
 
-
-int             thread_join(thread_handle hdl)
+int          thread_join(thread_t thd)
 {
-    int     status = 0;
+    int                 ret    = -1;
+    int                 status = OSA_SOK;
+    struct __thread_t * pthd   = (struct __thread_t *)thd;
 
-    DBG(DBG_DETAILED, GT_NAME, "thread_join: Enter\n");
+    DBG(DBG_DETAILED, GT_NAME, "thread_join: Enter.\n");
 
-    if (hdl == NULL) {
-        DBG(DBG_ERROR, GT_NAME, "thread_join: Invalid arguments\n");
-        return OSA_EINVAL;
-    }
+    thread_check_arguments(pthd);
 
-    status = pthread_join(hdl->thd_id, NULL);
+    status = pthread_join(pthd->thd_id, (void **)&ret);
 
-    DBG(DBG_DETAILED, GT_NAME, "thread_join: Exit (status=0x%x)\n", status);
+    DBG(DBG_DETAILED, GT_NAME, "thread_join: Exit (status=0x%x, ret=%d).\n", status, ret);
 
-    return status;
-
+    return status = OSA_SOK;
 }
 
-//int             thread_stat(thread_handle hdl);
+//int          thread_stat(thread_t thd);
 
-void            thread_yield(void)
+void         thread_yield(void)
 {
 
-    DBG(DBG_DETAILED, GT_NAME, "thread_cancel: Enter\n");
+    DBG(DBG_DETAILED, GT_NAME, "thread_yield: Enter.\n");
 
     sched_yield();
 
-    DBG(DBG_DETAILED, GT_NAME, "thread_cancel: Exit\n");
+    DBG(DBG_DETAILED, GT_NAME, "thread_yield: Exit.\n");
 }
-
 
 /*
  *  --------------------- Local function definition ----------------------------
@@ -377,32 +409,36 @@ void            thread_yield(void)
 
 /** ============================================================================
  *
- *  @Function:	    Local function definition.
+ *  @Function:      Local function definition.
  *
- *  @Description:   //	函数功能、性能等的描述
+ *  @Description:   // 函数功能、性能等的描述
  *
  *  ============================================================================
  */
-static void * __thread_run_stub(thread_handle hdl)
+static void * __thread_run_stub(struct __thread_t *pthd)
 {
-    pthread_setspecific(thd_key, hdl);
+    int status = 0;
 
-    pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
+    pthread_setspecific(glb_thd_key, pthd);
 
-    hdl->exit_status = (int)hdl->fxn(
-            hdl->args[0], hdl->args[1],hdl->args[2], hdl->args[3]
+    //pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
+    pthread_setcanceltype(PTHREAD_CANCEL_DEFERRED, NULL);
+    pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+
+    status = (int)pthd->fxn(
+            pthd->args[0], pthd->args[1], pthd->args[2], pthd->args[3]
             );
 
     /* TODO: Exit this thread */
     //pthread_exit(hdl);
 
-    return NULL;
+    return ((void *)status);
 }
 
 static void __thread_cleanup(void *hdl)
 {
-    if (--cur_init == 0) {
-        pthread_key_delete(thd_key);
+    if (--glb_cur_init == 0) {
+        pthread_key_delete(glb_thd_key);
     }
 }
 
