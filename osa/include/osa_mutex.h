7e062dde0d067c992b54377bf95615b4e892d287
xiong-kaifang <xiongkaifang87@163.com>
2015-09-19 16:14:36 +0800

    Rewrite osa_mutex and add osa_cond.

--- osa/include/osa_mutex.h
+++ osa/include/osa_mutex.h
@@ -6,24 +6,30 @@
  *
  *  @Author: xiong-kaifang   Version: v1.0   Date: 2012-02-24
  *
- *  @Description:   //	用于详细说明此程序文件完成的主要功能，与其它模块
- *		            //	或函数的接口，输出值，取值范围、含义及参数间的控
- *		            //	制、顺序、独立或依赖等关系
- *		            //
+ *  @Description:   // 用于详细说明此程序文件完成的主要功能，与其它模块
+ *                  // 或函数的接口，输出值，取值范围、含义及参数间的控
+ *                  // 制、顺序、独立或依赖等关系
+ *                  //
  *
- *	                The header file for osa mutex.
+ *                  The header file for osa mutex and osa cond.
  *
- *  @Others:	    //	其它内容说明
+ *  @Others:        // 其它内容说明
  *
- *  @Function List: //	主要函数列表，每条记录就包括函数名及功能简要说明
- *	    1.  ...
- *	    2.  ...
+ *  @Function List: // 主要函数列表，每条记录就包括函数名及功能简要说明
+ *      1.  ...
+ *      2.  ...
  *
- *  @History:	    //	修改历史记录列表，每条修改记录就包括修改日期、修改
- *	        	    //	时间及修改内容简述
+ *  @History:       // 修改历史记录列表，每条修改记录就包括修改日期、修改
+ *                  // 时间及修改内容简述
  *
- *	<author>	    <time>	     <version>	    <desc>
+ *  <author>        <time>       <version>      <description>
+ *
  *  xiong-kaifang   2012-02-24     v1.0	        Write this module.
+ *
+ *  xiong-kaifang   2015-09-19     v2.0         1. Add osa_cond_t related
+ *                                                  routines.
+ *                                              2. Using opaque type for
+ *                                                 osa_mutex_t and osa_cond_t.
  *
  *  ============================================================================
  */
@@ -32,9 +38,9 @@
 #define __OSA_MUTEX_H
 
 /*  --------------------- Include system headers ---------------------------- */
-#include <pthread.h>
 
 /*  --------------------- Include user headers   ---------------------------- */
+#include "std_defs.h"
 #include "osa_status.h"
 
 #if defined(__cplusplus)
@@ -51,13 +57,6 @@ extern "C" {
  *  @Description:   Description of this macro.
  *  ============================================================================
  */
-#define	MUTEX_INITIALIZER(mutex)	            \
-		(mutex.m_mutex = PTHREAD_MUTEX_INITIALIZER;)
-
-#define OSA_DECLARE_AND_INIT_MUTEX(mutex)       \
-    static mutex_t (mutex) = {                  \
-        .m_mutex = PTHREAD_MUTEX_INITIALIZER    \
-    };
 
 /*
  *  --------------------- Data type definition ---------------------------------
@@ -73,11 +72,9 @@ extern "C" {
  *  @Field:         Field2 member
  *  ----------------------------------------------------------------------------
  */
-struct __mutex_t; typedef struct __mutex_t mutex_t;
-struct __mutex_t
-{
-	pthread_mutex_t	m_mutex;
-};
+typedef HANDLE      osa_mutex_t;
+
+typedef HANDLE      osa_cond_t;
 
 /*
  *  --------------------- Public function declaration --------------------------
@@ -85,36 +82,43 @@ struct __mutex_t
 
 /** =============================================================================
  *
- *  @Function:	    //	函数名称
+ *  @Function:      // 函数名称
  *
- *  @Description:   //	函数功能、性能等的描述
+ *  @Description:   // 函数功能、性能等的描述
  *
- *  @Calls:	        //	被本函数调用的函数清单
+ *  @Calls:	        // 被本函数调用的函数清单
  *
- *  @Called By:	    //	调用本函数的函数清单
+ *  @Called By:	    // 调用本函数的函数清单
  *
- *  @Table Accessed://	被访问的表（此项仅对于牵扯到数据库操作的程序）
+ *  @Table Accessed:// 被访问的表（此项仅对于牵扯到数据库操作的程序）
  *
- *  @Table Updated: //	被修改的表（此项仅对于牵扯到数据库操作的程序）
+ *  @Table Updated: // 被修改的表（此项仅对于牵扯到数据库操作的程序）
  *
- *  @Input:	        //	对输入参数的说明
+ *  @Input:	        // 对输入参数的说明
  *
- *  @Output:	    //	对输出参数的说明
+ *  @Output:        // 对输出参数的说明
  *
- *  @Return:	    //	函数返回值的说明
+ *  @Return:        // 函数返回值的说明
  *
- *  @Enter          //  Precondition
+ *  @Enter          // Precondition
  *
- *  @Leave          //  Postcondition
+ *  @Leave          // Postcondition
  *
- *  @Others:	    //	其它说明
+ *  @Others:        // 其它说明
  *
  *  ============================================================================
  */
-status_t mutex_create(mutex_t *mutex);
-status_t mutex_lock  (mutex_t *mutex);
-status_t mutex_unlock(mutex_t *mutex);
-status_t mutex_delete(mutex_t *mutex);
+status_t osa_mutex_create(osa_mutex_t *pmutex);
+status_t osa_mutex_lock  (osa_mutex_t  mutex);
+status_t osa_mutex_unlock(osa_mutex_t  mutex);
+status_t osa_mutex_delete(osa_mutex_t *pmutex);
+
+status_t osa_cond_create (osa_cond_t *pcond);
+status_t osa_cond_wait   (osa_cond_t cond, osa_mutex_t mutex);
+status_t osa_cond_timedwait(osa_cond_t cond, osa_mutex_t mutex, unsigned int timeout);
+status_t osa_cond_signal (osa_cond_t cond);
+status_t osa_cond_broadcast(osa_cond_t cond);
+status_t osa_cond_delete (osa_cond_t *pcond);
 
 #if defined(__cplusplus)
 }
--- osa/src/osa_mutex.c
+++ osa/src/osa_mutex.c
@@ -7,26 +7,36 @@
  *  @Author: xiong-kaifang   Version: v1.0   Date: 2013-04-04
  *
  *  @Description:   The osa mutex.
- *	
  *
- *  @Version:	    v1.0
  *
- *  @Function List:  //	主要函数及功能
- *	    1.  －－－－－
- *	    2.  －－－－－
+ *  @Version:       v1.0
  *
- *  @History:	     //	历史修改记录
+ *  @Function List: // 主要函数及功能
+ *      1.  －－－－－
+ *      2.  －－－－－
  *
- *	<author>	    <time>	     <version>	    <desc>
+ *  @History:       // 历史修改记录
+ *
+ *  <author>        <time>       <version>      <description>
+ *
  *  xiong-kaifang   2013-04-04     v1.0	        Write this module.
  *
+ *  xiong-kaifang   2015-09-19     v2.0         1. Implement osa_cond_t related
+ *                                                 routines.
+ *                                              2. Dynamically create and delete
+ *                                                 osa_mutex_t and osa_cond_t.
+ *                                              3. Add codes to check arguments.
  *
  *  ============================================================================
  */
 
 /*  --------------------- Include system headers ---------------------------- */
+#include <time.h>
+#include <errno.h>
+#include <pthread.h>
 
 /*  --------------------- Include user headers   ---------------------------- */
+#include "osa.h"
 #include "osa_mutex.h"
 
 #if defined(__cplusplus)
@@ -43,6 +53,14 @@ extern "C" {
  *  @Description:   Description of this macro.
  *  ============================================================================
  */
+#define MILLIONS    (1000000)
+#define BILLIONS    (1000000000)
+
+#define osa_mutex_check_arguments(arg)          osa_check_arguments(arg)
+#define osa_mutex_check_arguments2(arg1, arg2)  osa_check_arguments2(arg1, arg2)
+
+#define osa_cond_check_arguments(arg)           osa_check_arguments(arg)
+#define osa_cond_check_arguments2(arg1, arg2)   osa_check_arguments2(arg1, arg2)
 
 /*
  *  --------------------- Structure definition ---------------------------------
@@ -58,6 +76,15 @@ extern "C" {
  *  @Field          Field2 member
  *  ----------------------------------------------------------------------------
  */
+struct __osa_mutex_t
+{
+    pthread_mutex_t m_mutex;
+};
+
+struct __osa_cond_t
+{
+    pthread_cond_t  m_cond;
+};
 
 /*
  *  --------------------- Global variable definition ---------------------------
@@ -76,7 +103,7 @@ extern "C" {
 
 /** ============================================================================
  *
- *  @Function:	    Local function forward declaration.
+ *  @Function:      Local function forward declaration.
  *
  *  @Description:   //	函数功能、性能等的描述
  *
@@ -90,15 +117,15 @@ extern "C" {
  *
  *  @Input:	        //	对输入参数的说明
  *
- *  @Output:	    //	对输出参数的说明
+ *  @Output:        //	对输出参数的说明
  *
- *  @Return:	    //	函数返回值的说明
+ *  @Return:        //	函数返回值的说明
  *
  *  @Enter          //  Precondition
  *
  *  @Leave          //  Postcondition
  *
- *  @Others:	    //	其它说明
+ *  @Others:        //	其它说明
  *
  *  ============================================================================
  */
@@ -109,68 +136,209 @@ extern "C" {
 
 /** ============================================================================
  *
- *  @Function:	    Public function definition.
+ *  @Function:    Public function definition.
  *
- *  @Description:   //	函数功能、性能等的描述
+ *  @Description:   // 函数功能、性能等的描述
  *
- *  @Calls:	        //	被本函数调用的函数清单
+ *  @Calls:	        // 被本函数调用的函数清单
  *
- *  @Called By:	    //	调用本函数的函数清单
+ *  @Called By:	    // 调用本函数的函数清单
  *
- *  @Table Accessed://	被访问的表（此项仅对于牵扯到数据库操作的程序）
+ *  @Table Accessed:// 被访问的表（此项仅对于牵扯到数据库操作的程序）
  *
- *  @Table Updated: //	被修改的表（此项仅对于牵扯到数据库操作的程序）
+ *  @Table Updated: // 被修改的表（此项仅对于牵扯到数据库操作的程序）
  *
- *  @Input:	        //	对输入参数的说明
+ *  @Input:	        // 对输入参数的说明
  *
- *  @Output:	    //	对输出参数的说明
+ *  @Output:        // 对输出参数的说明
  *
- *  @Return:	    //	函数返回值的说明
+ *  @Return:        // 函数返回值的说明
  *
- *  @Enter          //  Precondition
+ *  @Enter          // Precondition
  *
- *  @Leave          //  Postcondition
+ *  @Leave          // Postcondition
  *
- *  @Others:	    //	其它说明
+ *  @Others:        // 其它说明
  *
  *  ============================================================================
  */
-status_t mutex_create(mutex_t *mutex)
+status_t osa_mutex_create(osa_mutex_t *pmutex)
 {
-    status_t status = OSA_SOK;
-    pthread_mutexattr_t mutex_attr;
+    status_t               status = OSA_SOK;
+    pthread_mutexattr_t    mutex_attr;
+    struct __osa_mutex_t * pmtx   = NULL;
+
+    osa_mutex_check_arguments(pmutex);
+
+    (*pmutex) = INVALID_HANDLE;
+
+    status = OSA_memAlloc(sizeof(struct __osa_mutex_t), &pmtx);
+    if (OSA_ISERROR(status) || pmtx == NULL) {
+        return status;
+    }
 
     status |= pthread_mutexattr_init(&mutex_attr);
-    status |= pthread_mutex_init(&mutex->m_mutex, &mutex_attr);
+    status |= pthread_mutex_init(&pmtx->m_mutex, &mutex_attr);
 
     pthread_mutexattr_destroy(&mutex_attr);
 
-    return status;
-}
-
-status_t mutex_lock  (mutex_t *mutex)
-{
-    status_t status = OSA_SOK;
-
-    pthread_mutex_lock(&mutex->m_mutex);
+    (*pmutex) = (osa_mutex_t)pmtx;
 
     return status;
 }
 
-status_t mutex_unlock(mutex_t *mutex)
+status_t osa_mutex_lock  (osa_mutex_t mutex)
 {
-    status_t status = OSA_SOK;
+    status_t               status = OSA_SOK;
+    struct __osa_mutex_t * pmutex = (struct __osa_mutex_t *)mutex;
 
-    pthread_mutex_unlock(&mutex->m_mutex);
+    osa_mutex_check_arguments(pmutex);
+
+    pthread_mutex_lock(&pmutex->m_mutex);
 
     return status;
 }
 
-status_t mutex_delete(mutex_t *mutex)
+status_t osa_mutex_unlock(osa_mutex_t mutex)
 {
-    status_t status = OSA_SOK;
+    status_t               status = OSA_SOK;
+    struct __osa_mutex_t * pmutex = (struct __osa_mutex_t *)mutex;
 
-    pthread_mutex_destroy(&mutex->m_mutex);
+    osa_mutex_check_arguments(pmutex);
+
+    pthread_mutex_unlock(&pmutex->m_mutex);
+
+    return OSA_SOK;
+}
+
+status_t osa_mutex_delete(osa_mutex_t *pmutex)
+{
+    status_t               status = OSA_SOK;
+    struct __osa_mutex_t * pmtx   = (struct __osa_mutex_t *)(*pmutex);
+
+    osa_mutex_check_arguments2(pmutex, pmtx);
+
+    pthread_mutex_destroy(&pmtx->m_mutex);
+
+    status |= OSA_memFree(sizeof(struct __osa_mutex_t), pmtx);
+
+    (*pmutex) = INVALID_HANDLE;
+
+    return status;
+}
+
+/* osa cond */
+status_t osa_cond_create(osa_cond_t *pcond)
+{
+    status_t              status = OSA_SOK;
+    struct __osa_cond_t * pcnd   = NULL;
+
+    osa_cond_check_arguments(pcond);
+
+    (*pcond) = INVALID_HANDLE;
+
+    status = OSA_memAlloc(sizeof(struct __osa_cond_t), &pcnd);
+    if (OSA_ISERROR(status) || pcnd == NULL) {
+        return status;
+    }
+
+    pthread_cond_init(&pcnd->m_cond, NULL);
+
+    (*pcond) = (osa_cond_t)pcnd;
+
+    return status;
+}
+
+status_t osa_cond_wait  (osa_cond_t cond, osa_mutex_t mutex)
+{
+    status_t               status = OSA_SOK;
+    struct __osa_cond_t  * pcond  = (struct __osa_cond_t  *)cond;
+    struct __osa_mutex_t * pmutex = (struct __osa_mutex_t *)mutex;
+
+    osa_cond_check_arguments2(pcond, pmutex);
+
+    pthread_cond_wait(&pcond->m_cond, &pmutex->m_mutex);
+
+    return status;
+}
+
+status_t osa_cond_timedwait(osa_cond_t cond, osa_mutex_t mutex, unsigned int timeout)
+{
+    int                    retval;
+    status_t               status = OSA_SOK;
+    struct timespec        abstime;
+    struct __osa_cond_t  * pcond  = (struct __osa_cond_t  *)cond;
+    struct __osa_mutex_t * pmutex = (struct __osa_mutex_t *)mutex;
+
+    osa_cond_check_arguments2(pcond, pmutex);
+
+    /*
+     *  According to the Pthreads specification, we should use the absolute
+     *  timeout.
+     */
+#if 0
+    abstime.tv_sec   = timeout / 1000;
+    abstime.tv_nsec  = (timeout % 1000) * 1000000;
+#else
+    clock_gettime(CLOCK_REALTIME, &abstime);
+
+    abstime.tv_sec  += timeout / 1000;
+    abstime.tv_nsec += (timeout % 1000) * MILLIONS;
+    if (abstime.tv_nsec >= BILLIONS) {
+        abstime.tv_sec  -= BILLIONS;
+        abstime.tv_nsec += 1;
+    }
+#endif
+
+    retval = pthread_cond_timedwait(&pcond->m_cond, &pmutex->m_mutex, &abstime);
+
+    if (retval == 0) {
+        status = OSA_SOK;
+    } else if (retval == ETIMEDOUT) {
+        status = OSA_ETIMEOUT;
+    } else {
+        status = OSA_EINVAL;
+    }
+
+    return status;
+}
+
+status_t osa_cond_signal(osa_cond_t cond)
+{
+    status_t              status = OSA_SOK;
+    struct __osa_cond_t * pcond  = (struct __osa_cond_t *)cond;
+
+    osa_cond_check_arguments(pcond);
+
+    pthread_cond_signal(&pcond->m_cond);
+
+    return status;
+}
+
+status_t osa_cond_broadcast(osa_cond_t cond)
+{
+    status_t              status = OSA_SOK;
+    struct __osa_cond_t * pcond  = (struct __osa_cond_t *)cond;
+
+    osa_cond_check_arguments(pcond);
+
+    pthread_cond_broadcast(&pcond->m_cond);
+
+    return status;
+}
+
+status_t osa_cond_delete(osa_cond_t *pcond)
+{
+    status_t              status = OSA_SOK;
+    struct __osa_cond_t * pcnd   = (struct __osa_cond_t *)(*pcond);
+
+    osa_cond_check_arguments2(pcond, pcnd);
+
+    pthread_cond_destroy(&pcnd->m_cond);
+
+    status |= OSA_memFree(sizeof(struct __osa_cond_t), pcnd);
+
+    (*pcond) = INVALID_HANDLE;
 
     return status;
 }
@@ -181,9 +349,9 @@ status_t mutex_delete(mutex_t *mutex)
 
 /** ============================================================================
  *
- *  @Function:	    Local function definition.
+ *  @Function:      Local function definition.
  *
- *  @Description:   //	函数功能、性能等的描述
+ *  @Description:   // 函数功能、性能等的描述
  *
  *  ============================================================================
  */
