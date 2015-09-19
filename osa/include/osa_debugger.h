73f5d7f60d654b16a0ecbd26b6695c6d37d14f7c
xiong-kaifang <xiongkaifang87@163.com>
2015-09-19 17:08:59 +0800

    Using osa_mutex_t.

--- osa/include/osa_debugger.h
+++ osa/include/osa_debugger.h
@@ -6,23 +6,24 @@
  *
  *  @Author: xiong-kaifang   Version: v1.0   Date: 2013-11-20
  *
- *  @Description:   //	用于详细说明此程序文件完成的主要功能，与其它模块
- *		            //	或函数的接口，输出值，取值范围、含义及参数间的控
- *		            //	制、顺序、独立或依赖等关系
- *		            //
+ *  @Description:   // 用于详细说明此程序文件完成的主要功能，与其它模块
+ *                  // 或函数的接口，输出值，取值范围、含义及参数间的控
+ *                  // 制、顺序、独立或依赖等关系
+ *                  //
  *
- *	                The header file for osa debugger.
+ *                  The header file for osa debugger.
  *
- *  @Others:	    //	其它内容说明
+ *  @Others:        // 其它内容说明
  *
- *  @Function List: //	主要函数列表，每条记录就包括函数名及功能简要说明
- *	    1.  ...
- *	    2.  ...
+ *  @Function List: // 主要函数列表，每条记录就包括函数名及功能简要说明
+ *      1.  ...
+ *      2.  ...
  *
- *  @History:	    //	修改历史记录列表，每条修改记录就包括修改日期、修改
- *	        	    //	时间及修改内容简述
+ *  @History:       // 修改历史记录列表，每条修改记录就包括修改日期、修改
+ *                  // 时间及修改内容简述
  *
- *	<author>	    <time>	     <version>	    <desc>
+ *  <author>        <time>       <version>      <description>
+ *
  *  xiong-kaifang   2013-11-20     v1.0	        Write this module.
  *
  *  ============================================================================
@@ -61,8 +62,8 @@ extern "C" {
 #define DBG(level, tag, arg...) osa_debugger((level), (tag), ##arg);
 
 // For back compatience
-#define SUCCEEDED(r)	((r) >= 0)
-#define FAILED(r)		((r) < 0)
+#define SUCCEEDED(r)	(!OSA_ISERROR(r))
+#define FAILED(r)		(OSA_ISERROR(r))
 
 /*
  *  --------------------- Data type definition ---------------------------------
@@ -94,29 +95,29 @@ struct __osa_debugger_prm_t
 
 /** =============================================================================
  *
- *  @Function:	    //	函数名称
+ *  @Function:      // 函数名称
  *
- *  @Description:   //	函数功能、性能等的描述
+ *  @Description:   // 函数功能、性能等的描述
  *
- *  @Calls:	        //	被本函数调用的函数清单
+ *  @Calls:	        // 被本函数调用的函数清单
  *
- *  @Called By:	    //	调用本函数的函数清单
+ *  @Called By:	    // 调用本函数的函数清单
  *
- *  @Table Accessed://	被访问的表（此项仅对于牵扯到数据库操作的程序）
+ *  @Table Accessed:// 被访问的表（此项仅对于牵扯到数据库操作的程序）
  *
- *  @Table Updated: //	被修改的表（此项仅对于牵扯到数据库操作的程序）
+ *  @Table Updated: // 被修改的表（此项仅对于牵扯到数据库操作的程序）
  *
- *  @Input:	        //	对输入参数的说明
+ *  @Input:	        // 对输入参数的说明
  *
- *  @Output:	    //	对输出参数的说明
+ *  @Output:        // 对输出参数的说明
  *
- *  @Return:	    //	函数返回值的说明
+ *  @Return:        // 函数返回值的说明
  *
- *  @Enter          //  Precondition
+ *  @Enter          // Precondition
  *
- *  @Leave          //  Postcondition
+ *  @Leave          // Postcondition
  *
- *  @Others:	    //	其它说明
+ *  @Others:        // 其它说明
  *
  *  ============================================================================
  */
--- osa/src/osa_debugger.c
+++ osa/src/osa_debugger.c
@@ -7,19 +7,21 @@
  *  @Author: xiong-kaifang   Version: v1.0   Date: 2013-11-20
  *
  *  @Description:   The osa debugger.
- *	
  *
- *  @Version:	    v1.0
  *
- *  @Function List:  //	主要函数及功能
- *	    1.  －－－－－
- *	    2.  －－－－－
+ *  @Version:       v1.0
  *
- *  @History:	     //	历史修改记录
+ *  @Function List: // 主要函数及功能
+ *      1.  －－－－－
+ *      2.  －－－－－
  *
- *	<author>	    <time>	     <version>	    <desc>
+ *  @History:       // 历史修改记录
+ *
+ *  <author>        <time>       <version>      <description>
+ *
  *  xiong-kaifang   2013-11-20     v1.0	        Write this module.
  *
+ *  xiong-kaifang   2015-09-19     v1.1         Using osa_mutex_t.
  *
  *  ============================================================================
  */
@@ -50,15 +52,6 @@ extern "C" {
  *  @Description:   Description of this macro.
  *  ============================================================================
  */
-#define OSA_DEBUGGER_CRITICAL_ENTER()       \
-    do {                                    \
-        mutex_lock(&glb_debug_mutex);       \
-    } while (0)
-
-#define OSA_DEBUGGER_CRITICAL_LEAVE()       \
-    do {                                    \
-        mutex_unlock(&glb_debug_mutex);     \
-    } while (0)
 
 /*
  *  --------------------- Structure definition ---------------------------------
@@ -84,7 +77,7 @@ struct __osa_debugger_object_t
     FILE *          m_out;
     FILE *          m_out2;
     unsigned int    m_debug_level;
-    mutex_t         m_mutex;
+    osa_mutex_t     m_mutex;
     struct tm       m_tm;
 };
 
@@ -98,41 +91,40 @@ struct __osa_debugger_object_t
  *  @Description:   Description of the variable.
  * -----------------------------------------------------------------------------
  */
-static unsigned int glb_cur_init = 0;
+static unsigned int          glb_cur_init = 0;
 
 static osa_debugger_object_t glb_debug_obj;
 
-OSA_DECLARE_AND_INIT_MUTEX(glb_debug_mutex);
-
+static const char * const    GT_NAME = "osa_debugger";
 /*
  *  --------------------- Local function forward declaration -------------------
  */
 
 /** ============================================================================
  *
- *  @Function:	    Local function forward declaration.
+ *  @Function:      Local function forward declaration.
  *
- *  @Description:   //	函数功能、性能等的描述
+ *  @Description:   // 函数功能、性能等的描述
  *
- *  @Calls:	        //	被本函数调用的函数清单
+ *  @Calls:	        // 被本函数调用的函数清单
  *
- *  @Called By:	    //	调用本函数的函数清单
+ *  @Called By:	    // 调用本函数的函数清单
  *
- *  @Table Accessed://	被访问的表（此项仅对于牵扯到数据库操作的程序）
+ *  @Table Accessed:// 被访问的表（此项仅对于牵扯到数据库操作的程序）
  *
- *  @Table Updated: //	被修改的表（此项仅对于牵扯到数据库操作的程序）
+ *  @Table Updated: // 被修改的表（此项仅对于牵扯到数据库操作的程序）
  *
- *  @Input:	        //	对输入参数的说明
+ *  @Input:	        // 对输入参数的说明
  *
- *  @Output:	    //	对输出参数的说明
+ *  @Output:        // 对输出参数的说明
  *
- *  @Return:	    //	函数返回值的说明
+ *  @Return:        // 函数返回值的说明
  *
- *  @Enter          //  Precondition
+ *  @Enter          // Precondition
  *
- *  @Leave          //  Postcondition
+ *  @Leave          // Postcondition
  *
- *  @Others:	    //	其它说明
+ *  @Others:        // 其它说明
  *
  *  ============================================================================
  */
@@ -149,55 +141,51 @@ static void     __osa_debugger_update(struct tm *ptm);
 
 /** ============================================================================
  *
- *  @Function:	    Public function definition.
+ *  @Function:      Public function definition.
  *
- *  @Description:   //	函数功能、性能等的描述
+ *  @Description:   // 函数功能、性能等的描述
  *
- *  @Calls:	        //	被本函数调用的函数清单
+ *  @Calls:	        // 被本函数调用的函数清单
  *
- *  @Called By:	    //	调用本函数的函数清单
+ *  @Called By:	    // 调用本函数的函数清单
  *
- *  @Table Accessed://	被访问的表（此项仅对于牵扯到数据库操作的程序）
+ *  @Table Accessed:// 被访问的表（此项仅对于牵扯到数据库操作的程序）
  *
- *  @Table Updated: //	被修改的表（此项仅对于牵扯到数据库操作的程序）
+ *  @Table Updated: // 被修改的表（此项仅对于牵扯到数据库操作的程序）
  *
- *  @Input:	        //	对输入参数的说明
+ *  @Input:	        // 对输入参数的说明
  *
- *  @Output:	    //	对输出参数的说明
+ *  @Output:        // 对输出参数的说明
  *
- *  @Return:	    //	函数返回值的说明
+ *  @Return:        // 函数返回值的说明
  *
- *  @Enter          //  Precondition
+ *  @Enter          // Precondition
  *
- *  @Leave          //  Postcondition
+ *  @Leave          // Postcondition
  *
- *  @Others:	    //	其它说明
+ *  @Others:        // 其它说明
  *
  *  ============================================================================
  */
 status_t osa_debugger_init(const osa_debugger_prm_t *prm)
 {
-    status_t status = OSA_SOK;
-    osa_debugger_object_t *pobj = &glb_debug_obj;
-
-    OSA_DEBUGGER_CRITICAL_ENTER();
+    status_t                status = OSA_SOK;
+    osa_debugger_object_t * pobj   = &glb_debug_obj;
 
     if (glb_cur_init++ == 0) {
         status = __osa_debugger_init(pobj, prm);
     }
-
-    OSA_DEBUGGER_CRITICAL_LEAVE();
 
     return status;
 }
 
 void     osa_debugger(int level, const char *tags, const char *fmt, ...)
 {
-    osa_debugger_object_t *pobj = &glb_debug_obj;
+    osa_debugger_object_t * pobj = &glb_debug_obj;
 
     if (level >= pobj->m_debug_level) {
 
-        mutex_lock(&pobj->m_mutex);
+        osa_mutex_lock(pobj->m_mutex);
 
         struct tm tim;
         struct timeval tv;
@@ -228,41 +216,37 @@ void     osa_debugger(int level, const char *tags, const char *fmt, ...)
 
         __osa_debugger_update(&tim);
 
-        mutex_unlock(&pobj->m_mutex);
+        osa_mutex_unlock(pobj->m_mutex);
     }
 }
 
 status_t osa_debugger_setlevel(int level)
 {
-    osa_debugger_object_t *pobj = &glb_debug_obj;
+    osa_debugger_object_t * pobj = &glb_debug_obj;
 
     if (level < DBG_DETAILED || level > DBG_FATAL) {
         return OSA_EINVAL;
     }
 
-    mutex_lock(&pobj->m_mutex);
+    osa_mutex_lock(pobj->m_mutex);
 
     pobj->m_debug_level = level;
 
-    mutex_unlock(&pobj->m_mutex);
+    osa_mutex_unlock(pobj->m_mutex);
 
-    DBG(DBG_ERROR, "osa debugger", "Logger system set debug level=%d.\n", pobj->m_debug_level);
+    DBG(DBG_ERROR, GT_NAME, "Logger system set log level=%d.\n", pobj->m_debug_level);
 
     return OSA_SOK;
 }
 
 status_t osa_debugger_deinit(void)
 {
-    status_t status = OSA_SOK;
-    osa_debugger_object_t *pobj = &glb_debug_obj;
-
-    OSA_DEBUGGER_CRITICAL_ENTER();
+    status_t                status = OSA_SOK;
+    osa_debugger_object_t * pobj   = &glb_debug_obj;
 
     if (--glb_cur_init == 0) {
         status = __osa_debugger_deinit(pobj);
     }
-
-    OSA_DEBUGGER_CRITICAL_LEAVE();
 
     return status;
 }
@@ -273,9 +257,9 @@ status_t osa_debugger_deinit(void)
 
 /** ============================================================================
  *
- *  @Function:	    Local function definition.
+ *  @Function:      Local function definition.
  *
- *  @Description:   //	函数功能、性能等的描述
+ *  @Description:   // 函数功能、性能等的描述
  *
  *  ============================================================================
  */
@@ -288,16 +272,18 @@ __osa_debugger_init(osa_debugger_object_t *pobj, const osa_debugger_prm_t *prm)
 
     memcpy(&pobj->m_debug_prm, prm, sizeof(*prm));
 
-    pobj->m_out = prm->m_out;
+    pobj->m_out         = prm->m_out;
     pobj->m_debug_level = prm->m_debug_level;
-    status |=mutex_create(&pobj->m_mutex);
 
-    OSA_assert(OSA_SOK == status);
+    status = osa_mutex_create(&pobj->m_mutex);
+    if (OSA_ISERROR(status)) {
+        return status;
+    }
 
     if (pobj->m_debug_prm.m_folder != NULL) {
         unsigned char filename[128];
-        time_t now_time;
-        struct tm tim;
+        time_t        now_time;
+        struct tm     tim;
 
         time(&now_time);
         localtime_r(&now_time, &tim);
@@ -310,7 +296,7 @@ __osa_debugger_init(osa_debugger_object_t *pobj, const osa_debugger_prm_t *prm)
         pobj->m_out2 = fopen(filename, "ab");
 
         if (pobj->m_out2 != NULL) {
-            osa_debugger(DBG_ERROR, "osa debugger", "Logger system started.\n");
+            osa_debugger(DBG_ERROR, GT_NAME, "Logger system started.\n");
         }
     }
 
@@ -322,22 +308,22 @@ __osa_debugger_deinit(osa_debugger_object_t *pobj)
 {
     status_t status = OSA_SOK;
 
-    if (pobj->m_out2 != NULL) {
-        DBG(DBG_ERROR, "osa debugger", "Logger system stopped.\n\n");
+    if (pobj->m_out  != NULL) {
+        DBG(DBG_ERROR, GT_NAME, "Logger system stopped.\n\n");
     }
 
     if (pobj->m_out2 != NULL) {
         fclose(pobj->m_out2);
     }
 
-    status |= mutex_delete(&pobj->m_mutex);
+    status = osa_mutex_delete(&pobj->m_mutex);
 
     return status;
 }
 
 static void __osa_debugger_update(struct tm *ptm)
 {
-    osa_debugger_object_t *pobj = &glb_debug_obj;
+    osa_debugger_object_t * pobj = &glb_debug_obj;
 
 	if(pobj->m_out2 == NULL) {
 		return;
